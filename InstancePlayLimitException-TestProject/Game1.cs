e8c4aefb1f67635bb4abcf1bfe023c85de1761ca
HarriOrri <haraldur1234@gmail.com>
2022-05-01 17:09:29 +0200

    Initial commit

---
+++ InstancePlayLimitException-TestProject/Game1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace InstancePlayLimitException_TestProject
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private SoundEffect _badSound;
+        private SoundEffectInstance _badSoundInstance;
+        private double _soundDelay;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            _soundDelay = 0.0;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _badSound = this.Content.Load<SoundEffect>("sound");
+            _badSoundInstance = _badSound.CreateInstance();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            _soundDelay -= gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_soundDelay < 0.0)
+            {
+                _badSoundInstance.Stop();
+                _badSoundInstance.Play();
+                _soundDelay = 22.0 / 30.0;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ InstancePlayLimitException-TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InstancePlayLimitException_TestProject
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
